ee8561197efdea284a7434a584dd63daa8592b53
Matheus S. Barros <matheus.eco.2010@gmail.com>
2018-11-03 21:42:50 +0100

    Uploading repository

--- material/lecture14-start/includes/header.php
+++
@@ -1,9 +0,0 @@
-    <head>
-        <meta charset="UTF-8">
-        <title>ImaginaryEnv</title>
-        <link rel="stylesheet" href="/<?php echo $root_directory ?>/css/bootstrap.css">
-        <link rel="stylesheet" href="/<?php echo $root_directory ?>/css/style.css">
-        <script src="/<?php  echo $root_directory?>/resources/jquery.min.js"></script>
-        <script src="/<?php  echo $root_directory?>/js/js_functions.js"></script>
-    </head>
-   
--- material/lecture15-start/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "includes/init.php";
-    unset($_SESSION['username']);
-    if (isset($_COOKIE['username'])) {
-        setcookie('username', 'delete', time()-3600);
-    }
-    redirect('index.php');
-?>
